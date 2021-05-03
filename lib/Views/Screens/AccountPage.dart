a9483f73117efd31d736a24dcf327492c349024a
3abrazek <mohamedabdelrazek824@gmail.com>
2021-05-03 08:16:23 +0200

    AccountPage.dart Logged Check

--- lib/Views/Screens/AccountPage.dart
+++ lib/Views/Screens/AccountPage.dart
@@ -1,10 +1,108 @@
+import 'package:events_app/Controllers/UserCredentialController.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class AccountPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Provider.of<UserInfoController>(context).username != null ? Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          CircleAvatar(
+            minRadius: width*.3,
+            backgroundImage: AssetImage('images/me.jpg'),
+          ),
+          Column(
+            children: [
+              UserDataCard(width: width, height: height,text: Provider.of<UserInfoController>(context).username,myFunc: (){},icon: Icons.account_circle,),
+              UserDataCard(width: width, height: height,text:  Provider.of<UserInfoController>(context).phone,myFunc: (){},icon: Icons.phone,),
+            ],
+          ),
+        ],
+      ),
+    ) : Scaffold(
+          body: Center(
+            child: InkWell(
+              onTap: (){
+                Navigator.pop(context);
+              },
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image(
+                    image: AssetImage('images/login.png'),
+                    width: width*.7,
+                  ),
+                  Text(
+                    'You Need To Login',
+                    style: TextStyle(
+                      color: Colors.black.withOpacity(.8),
+                      fontWeight: FontWeight.bold,
+                      fontSize: height*.04
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+  }
+}
+
+class UserDataCard extends StatelessWidget {
+  const UserDataCard({
+    @required this.width,
+    @required this.height,
+    @required this.icon,
+    @required this.myFunc,
+    @required this.text,
+
+  });
+
+  final double width;
+  final double height;
+  final IconData icon;
+  final Function myFunc;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      color: Colors.black,
+      width: width*.8,
+      height: height*.08,
+      margin: EdgeInsets.only(bottom: height*.02),
+      padding: EdgeInsets.symmetric(horizontal: width*.04),
+      decoration: BoxDecoration(
+        color: Colors.grey.shade300,
+        borderRadius: BorderRadius.circular(width*.5)
+      ),
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Icon(icon,color: Colors.black,size: height*.05,),
+                SizedBox(width: width*.03,),
+                Text(
+                  text,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: width*.04,
+                  ),
+                ),
+              ],
+            ),
+            GestureDetector(
+              onTap: myFunc,
+              child: Icon(Icons.drive_file_rename_outline,color: Colors.blue,size: height*.03,),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/Views/Screens/AddEventScreen.dart
+++ lib/Views/Screens/AddEventScreen.dart
@@ -1,6 +1,7 @@
 import 'package:events_app/Controllers/AddEventController.dart';
 import 'package:events_app/Controllers/ImagePickerController.dart';
 import 'package:events_app/Controllers/MyLocationController.dart';
+import 'package:events_app/Controllers/UserCredentialController.dart';
 import 'package:events_app/Services/ImageStorage.dart';
 import 'package:events_app/Views/Component/MyTextField.dart';
 import 'package:flutter/cupertino.dart';
@@ -24,7 +25,7 @@ class _AddEventScreenState extends State<AddEventScreen> {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
-    return GestureDetector(
+    return Provider.of<UserInfoController>(context).username != null ? GestureDetector(
       onTap: (){
         FocusScope.of(context).requestFocus(FocusNode());
       },
@@ -32,45 +33,45 @@ class _AddEventScreenState extends State<AddEventScreen> {
         body: Stack(
           children:[
             Container(
-            padding: EdgeInsets.only(
-                left: width * .05,right: width * .05),
-            child: ListView(
-              physics: BouncingScrollPhysics(),
-              children: [
-                SizedBox(height: height*.05,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Add Event',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: height * .05,
-                          fontWeight: FontWeight.w300),
-                    ),
-                  ],
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: width * .2),
-                  child: Divider(
-                    color: Colors.blue,
+              padding: EdgeInsets.only(
+                  left: width * .05,right: width * .05),
+              child: ListView(
+                physics: BouncingScrollPhysics(),
+                children: [
+                  SizedBox(height: height*.05,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Add Event',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: height * .05,
+                            fontWeight: FontWeight.w300),
+                      ),
+                    ],
                   ),
-                ),
-                SizedBox(
-                  height: height * .02,
-                ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: width * .2),
+                    child: Divider(
+                      color: Colors.blue,
+                    ),
+                  ),
+                  SizedBox(
+                    height: height * .02,
+                  ),
 
-                Text(
-                  'Title',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: height * .04,
-                      fontWeight: FontWeight.w300),
-                ),
-                SizedBox(
-                  height: height * .02,
-                ),
-                MyTextField(
+                  Text(
+                    'Title',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: height * .04,
+                        fontWeight: FontWeight.w300),
+                  ),
+                  SizedBox(
+                    height: height * .02,
+                  ),
+                  MyTextField(
                     myColor: Colors.blue,
                     myController: _titleController,
                     height: height,
@@ -78,153 +79,153 @@ class _AddEventScreenState extends State<AddEventScreen> {
                     myWidth: .8,
                     myHeight: .1,
                     hint: 'Event Title',
-                ),
-                SizedBox(
-                  height: height * .02,
-                ),
-
-                Text(
-                  'About',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: height * .04,
-                      fontWeight: FontWeight.w300),
-                ),
-                SizedBox(
-                  height: height * .02,
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: width * .05),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(height * .02)),
-                  child: Center(
-                    child: TextField(
-                      controller: _summaryController,
-                      keyboardType: TextInputType.multiline,
-                      maxLines: 15,
-                      cursorColor: Colors.black87,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        enabledBorder: InputBorder.none,
-                        errorBorder: InputBorder.none,
-                        disabledBorder: InputBorder.none,
-                        hintText: 'About Your Event',
-                        contentPadding: EdgeInsets.only(
-                            left: 15, bottom: 30, top: 30, right: 15),
-                      ),
-                    ),
                   ),
-                ),
-                SizedBox(
-                  height: height * .02,
-                ),
-
-                Text(
-                  'Date',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: height * .04,
-                      fontWeight: FontWeight.w300),
-                ),
-                SizedBox(
-                  height: height * .02,
-                ),
-                TextButton(
-                onPressed: () {
-                     DatePicker.showDatePicker(context,
-                         showTitleActions: true,
-                         minTime: DateTime.now(),
-                         maxTime: DateTime(2030, 1, 1), onChanged: (date) {
-                           Provider.of<AddEventController>(context,listen: false).dateSetter(date);
-                         }, onConfirm: (date) {
-                         }, currentTime: DateTime.now(), locale: LocaleType.en);
-                },
-                child: Container(
-                  width: width,
-                  height: height*.1,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15)
+                  SizedBox(
+                    height: height * .02,
                   ),
-                  child: Center(
-                    child: Text(
-                      Provider.of<AddEventController>(context).eventDate.toString().replaceRange(10,Provider.of<AddEventController>(context).eventDate.toString().length, ''),
-                      style: TextStyle(color: Colors.blue),
-                    ),
-                  ),
-                ),),
-                SizedBox(
-                  height: height * .02,
-                ),
 
-                Text(
-                  'Location',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: height * .04,
-                      fontWeight: FontWeight.w300),
-                ),
-                SizedBox(
-                  height: height * .02,
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/MyMapPicker');
-                    //TODO Pick location
-                  },
-                  child: Container(
-                    width: width,
-                    height: height*.1,
+                  Text(
+                    'About',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: height * .04,
+                        fontWeight: FontWeight.w300),
+                  ),
+                  SizedBox(
+                    height: height * .02,
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: width * .05),
                     decoration: BoxDecoration(
                         color: Colors.white,
-                        borderRadius: BorderRadius.circular(15)
-                    ),
+                        borderRadius: BorderRadius.circular(height * .02)),
                     child: Center(
-                      child: Text(Provider.of<MyLocationController>(context).selectionState == true ? 'Place Picked' : 'Pick Place',
-                        style: TextStyle(color: Colors.blue),
+                      child: TextField(
+                        controller: _summaryController,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: 15,
+                        cursorColor: Colors.black87,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                          hintText: 'About Your Event',
+                          contentPadding: EdgeInsets.only(
+                              left: 15, bottom: 30, top: 30, right: 15),
+                        ),
                       ),
                     ),
-                  ),),
-                SizedBox(
-                  height: height * .02,
-                ),
+                  ),
+                  SizedBox(
+                    height: height * .02,
+                  ),
 
-                Text(
-                  'Poster',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: height * .04,
-                      fontWeight: FontWeight.w300),
-                ),
-                SizedBox(
-                  height: height * .02,
-                ),
-                TextButton(
-                  onPressed: () async {
-                    await Provider.of<ImagePickerController>(context,listen: false).getImage();
-                  },
-                  child: Container(
-                    width: width,
-                    height: height*.1,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(15)
-                    ),
-                    child: Center(
-                      child: Text(
-                        Provider.of<ImagePickerController>(context).image == null ? 'Pick Poster' : 'Image Selected',
-                        style: TextStyle(color: Colors.blue),
+                  Text(
+                    'Date',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: height * .04,
+                        fontWeight: FontWeight.w300),
+                  ),
+                  SizedBox(
+                    height: height * .02,
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      DatePicker.showDatePicker(context,
+                          showTitleActions: true,
+                          minTime: DateTime.now(),
+                          maxTime: DateTime(2030, 1, 1), onChanged: (date) {
+                            Provider.of<AddEventController>(context,listen: false).dateSetter(date);
+                          }, onConfirm: (date) {
+                          }, currentTime: DateTime.now(), locale: LocaleType.en);
+                    },
+                    child: Container(
+                      width: width,
+                      height: height*.1,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15)
                       ),
-                    ),
-                  ),),
-                SizedBox(
-                  height: height * .16,
-                ),
-              ],
+                      child: Center(
+                        child: Text(
+                          Provider.of<AddEventController>(context).eventDate.toString().replaceRange(10,Provider.of<AddEventController>(context).eventDate.toString().length, ''),
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      ),
+                    ),),
+                  SizedBox(
+                    height: height * .02,
+                  ),
+
+                  Text(
+                    'Location',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: height * .04,
+                        fontWeight: FontWeight.w300),
+                  ),
+                  SizedBox(
+                    height: height * .02,
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/MyMapPicker');
+                      //TODO Pick location
+                    },
+                    child: Container(
+                      width: width,
+                      height: height*.1,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15)
+                      ),
+                      child: Center(
+                        child: Text(Provider.of<MyLocationController>(context).selectionState == true ? 'Place Picked' : 'Pick Place',
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      ),
+                    ),),
+                  SizedBox(
+                    height: height * .02,
+                  ),
+
+                  Text(
+                    'Poster',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: height * .04,
+                        fontWeight: FontWeight.w300),
+                  ),
+                  SizedBox(
+                    height: height * .02,
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      await Provider.of<ImagePickerController>(context,listen: false).getImage();
+                    },
+                    child: Container(
+                      width: width,
+                      height: height*.1,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15)
+                      ),
+                      child: Center(
+                        child: Text(
+                          Provider.of<ImagePickerController>(context).image == null ? 'Pick Poster' : 'Image Selected',
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      ),
+                    ),),
+                  SizedBox(
+                    height: height * .16,
+                  ),
+                ],
+              ),
             ),
-          ),
             Positioned(
               top: height*.87,
               width: width,
@@ -256,6 +257,31 @@ class _AddEventScreenState extends State<AddEventScreen> {
                 ),),
             ),
           ],
+        ),
+      ),
+    ) : Scaffold(
+      body: Center(
+        child: InkWell(
+          onTap: (){
+            Navigator.pop(context);
+          },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image(
+                image: AssetImage('images/login.png'),
+                width: width*.7,
+              ),
+              Text(
+                'You Need To Login',
+                style: TextStyle(
+                    color: Colors.black.withOpacity(.8),
+                    fontWeight: FontWeight.bold,
+                    fontSize: height*.04
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
