90878f751f4ecb735b3c1bef5b0093ce9c848069
3abrazek <mohamedabdelrazek824@gmail.com>
2021-05-05 07:37:09 +0200

    EventModel

---
+++ lib/Model/EventData.dart
@@ -0,0 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class EventData {
+  EventData({
+    @required this.name,
+    @required this.about,
+    @required this.date,
+    @required this.xAxis,
+    @required this.yAxis,
+    @required this.imageUrl,
+    @required this.host,
+  });
+
+  String name;
+  String about;
+  String date;
+  String xAxis;
+  String yAxis;
+  String imageUrl;
+  String host;
+
+  CollectionReference _collectionReference =
+      FirebaseFirestore.instance.collection('events');
+
+  Future<void> addEvent() {
+    return _collectionReference
+        .doc(date)
+        .set({
+          'name'    : this.name,
+          'about'   : this.about,
+          'date'    : this.date,
+          'xAxis'   : this.xAxis,
+          'yAxis'   : this.yAxis,
+          'imageUrl': this.imageUrl,
+          'host'    : this.host,
+        })
+        .then((value) => print("Event Added"))
+        .catchError((error) => print("Failed to add event: $error"));
+  }
+}
