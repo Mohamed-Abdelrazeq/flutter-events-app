8229fb8ca01f5152df3f4527191028cedad6ad32
3abrazek <mohamedabdelrazek824@gmail.com>
2021-05-05 07:37:22 +0200

    EventModel

--- lib/Model/EventData.dart
+++ lib/Model/EventData.dart
@@ -1,8 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class EventData {
-  EventData({
+class EventModel {
+  EventModel({
     @required this.name,
     @required this.about,
     @required this.date,
